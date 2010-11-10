28153db4710e979e0d5ed24f85e85166d78008c6
Tapio Auvinen <tsauvine@cc.hut.fi>
2010-11-10 16:16:41 +0200

    Updated syntax fo 2.3.9.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,6 +25,9 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "shoulda", :lib => "shoulda"
+  config.gem "factory_girl", :version => '1.2.4'
+
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
@@ -48,7 +51,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_rubyric_session',
+    :key => '_rubyric_session',
     :secret      => '279282ee67bda79be8f011f06845e6462ebafdab8c883fb0b5f9bdcbce408c8f11d97ae352c4dd4a2ceded6089c54f68d4cf439266ff0bfb603ed7ca4b9d523b'
   }
 
