023685f09d2d584ff508aaa56e46c03b405c230f
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-01-25 15:39:48 +0200

    Refactoring invitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,14 +1,11 @@
 class Invitation < ActiveRecord::Base
   before_create :generate_token
   
+  # Generates a unique token
   def generate_token
-    # Generate a unique token
-    while true
-      token = Digest::SHA1.hexdigest([Time.now, rand].join)
-      break unless Invitation.exists?(:token => token)
-    end
-    
-    self.token = token
+    begin
+      self.token = Digest::SHA1.hexdigest([Time.now, rand].join)
+    end while Invitation.exists?(:token => self.token)
   end
 
 end
