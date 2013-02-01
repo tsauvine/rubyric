0ca8b6820ebe1fb1c061056da7da3ee27aa6b27f
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-02-01 16:20:34 +0200

    Removing hard-coded Shibboleth paths

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rubyric::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rubyric::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
 
   # Enable threaded mode
   # config.threadsafe!
