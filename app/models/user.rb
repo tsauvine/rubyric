db7a4eb5ac87d8a4cff762e594bacb29fb535c57
Tapio Auvinen <tsauvine@gmail.com>
2013-02-04 22:46:24 +0200

    Fixing user session issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
+    c.session_class = Session
     c.login_field = :email
     #c.validate_password_field = false
     #c.validate_email_field = false
