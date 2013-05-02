72262e2c7ffda048ff2a687a69faccce0e7778c2
Tapio Auvinen <tapio.auvinen@aalto.fi>
2013-05-02 14:51:35 +0300

    Show active link in left nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns 'active' if current controller and action match parameters. Otherwise, returns an empty string.
+  # e.g. active_nav('course', 'show')
+  def active_nav(target_controller, target_action)
+    puts "*****************"
+    puts controller.controller_name
+    puts controller.action_name
+    controller.controller_name == target_controller && controller.action_name == target_action ? 'active' : ''
+  end
+
 end
