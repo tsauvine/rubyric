e8a3d4d5b359562903215ff1577ea188fedd5bac
Tapio Auvinen <tapio.auvinen@aalto.fi>
2014-03-20 16:19:49 +0200

    Enable transition from SHA512 to SCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = :email
+    c.crypto_provider = Authlogic::CryptoProviders::SCrypt
+    c.transition_from_crypto_providers = Authlogic::CryptoProviders::Sha512
     #c.validate_password_field = false
     #c.validate_email_field = false
     #c.transition_from_restful_authentication = true
