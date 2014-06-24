cbe23862953b7be885cf96508a1914d0d891c32a
Tapio Auvinen <tsauvine@gmail.com>
2014-06-24 23:14:36 +0300

    Bump asset version number

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Rubyric
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.21'
+    config.assets.version = '1.22'
   end
 end
