10ab13fd5203524fdac95efb61547735f48e5a98
Tapio Auvinen <tsauvine@gmail.com>
2016-02-23 03:03:58 +0200

    Polishing collaborative view

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Rubyric
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.41'
+    config.assets.version = '1.42'
   end
 end
