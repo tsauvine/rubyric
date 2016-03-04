d198d3dbd2a8ce37d3f9f9e2e0c780534a78a9bb
Tapio Auvinen <tapio.auvinen@aalto.fi>
2016-03-04 03:33:23 +0200

    Send feedback and grades to A+

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -130,7 +130,7 @@ class FeedbackMailer < ActionMailer::Base
       feedback = render_to_string(action: :aplus)
     end
     
-    logger.info "Submission #{submission.id} (#{submission.aplus_feedback_url})"
+    logger.info "Submission #{submission.id} (#{submission.aplus_feedback_url})\n#{{points: combined_grade, max_points: max_grade, feedback: feedback}}"
     response = RestClient.post(submission.aplus_feedback_url, {points: combined_grade, max_points: max_grade, feedback: feedback})
     #logger.debug("Submission #{submission.id}: #{combined_grade}/#{max_grade}. #{feedback}")
     
