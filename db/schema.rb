e3de5b510ef4a004ad83722cf919391c196413c6
Kha Nguyen <nlhkha@gmail.com>
2016-11-08 10:47:08 +0200

    fix column name status in table reviews

--- db/schema.rb
+++ db/schema.rb
@@ -264,7 +264,7 @@ ActiveRecord::Schema.define(:version => 20161104175805) do
     t.integer  "submission_id"
     t.text     "feedback"
     t.string   "grade"
-    t.string   "state"
+    t.string   "status"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.integer  "calculated_grade"
